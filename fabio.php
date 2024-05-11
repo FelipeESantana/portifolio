224dce9dd30617eb096c8f22d30d42368e5458c9
FelipeESantana <124594414+FelipeESantana@users.noreply.github.com>
2024-05-11 11:25:08 -0300

    commit inicial

---
+++ fabio.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="port.css">
+    <title>Irmao</title>
+</head>
+<body>
+    <img src="eu-e-eu.jpeg" class="eu">
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="port.css">
+    <link rel="stylesheet" href="reset.css">
+    <title>Meu Portifolio</title>
+</head>
+<body>
+    <header>
+        <div class="topo">
+            <nav class="titulo">
+            <a>Quem Eu Sou?</a>
+            </nav>
+        
+        <img src="eu.jpeg" id="minhaimg">
+        </div>
+    </header>
+    <main>
+
+        <div class="central">
+
+            <ul class="texto">
+                
+                <li>Meu nome é Felipe Eduardo santana(tambem conhecido como crtl v)</li>
+                <li>Tenho 17 anos e estou no 3°ano do ensino medio</li>
+                <li>Gosto de ler, jogar, estudar e sou critico como hobby</li>
+                <li>Estudo programação no semear e no colégio 
+                    no ensino médio tecnico em desinvolvimento de sistemas</li>
+                 
+            </ul>
+            <nav class="lista">        
+                <a>Minhas capacitações:</a>
+                <li>HTML e CSS</li>
+                <li>JavaScript</li>
+                <li>C</li>
+                <li>Python</li>
+                <li>Programação em Arduino</li>
+                <li>Word,PowerPoint e Exel</li>
+                
+            </nav>
+                
+            
+        </div>
+        
+    </main>
+    <footer>
+      <nav class="final">
+        <a>Para entrar em contato:</a>
+        <li>telefone: (45)99142-6081</li>
+        <li>Email: felipesantanaau00@gmail.com</li>
+        <li>github:<a href="https://github.com/FelipeESantana">FelipeESantana</a></li>
+        <li>Instagram: felipe_eduard09</li>
+        <p class="fabio">E para quem nao sabe sou irmão gemeo desse animal aqui ==><a href="fabio.php">Fábio:)</a></p>
+      </nav>
+
+    </footer>
+    
+</body>
+</html>
