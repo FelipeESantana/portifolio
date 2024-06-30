69765629237f7f9252e80358e2db680f4c0c7f6c
FelipeESantana <124594414+FelipeESantana@users.noreply.github.com>
2024-06-30 18:24:34 -0300

    json projetos

--- index.php
+++ index.php
@@ -5,8 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="port.css">
-    
+    <link rel="stylesheet" href="meusprojetos.css">
     <title>Meu Portifolio</title>
+    <script>
+        function construirProjetos(){
+            const jasao = `[
+                {
+                "projetoNome":"MONTASK",
+                "projetoIMG":"IMGmontask.jpeg",
+                "projetoDesc": "O projeto montask é um site organizador de tarefas basico",
+                "projetoLink": "https://github.com/dashboard"
+                },
+                {
+                "projetoNome":"PORTIFOLIO",
+                "projetoIMG":"portfolio.png",
+                "projetoDesc": "Este proprio projeto em que voce esta e demonstra minhas capacidade em programação",
+                "projetoLink": "https://github.com/FelipeESantana/portifolio"
+                }
+                ]`;
+                let objetoProjetos = JSON.parse(jasao);
+                //cria o template original para ser copiado abaixo
+                const template = document.getElementById('objetoTemplate');
+                // Adiciona o objeto ao container de objetos
+                const container = document.getElementById('objetosContainer');
+                //forech para fazer o loop que passara pelo json
+                objetoProjetos.forEach(projeto =>{ 
+                // Clona o template
+                const objetoClone = document.importNode(template.content, true);
+
+                // Preenche os dados do objeto
+                objetoClone.querySelector('.objetoNome').textContent = projeto.projetoNome;
+                objetoClone.querySelector('.objetoDesc').textContent = projeto.projetoDesc;
+                objetoClone.querySelector('.objetoLink').textContent = projeto.projetoLink;
+                objetoClone.querySelector('.objetoLink').href = projeto.projetoLink;
+                objetoClone.querySelector('#objetoImagem').src = projeto.projetoIMG;
+                container.appendChild(objetoClone);
+                })//termina o forEach
+                
+        }
+        document.addEventListener('DOMContentLoaded', function() {
+            construirProjetos();
+        });
+    </script>
 </head>
 <body>
     <header>
@@ -18,6 +58,7 @@
         <img src="eu.jpeg" id="minhaimg" alt="">
 
         </div>
+
     </header>
     <main>
 
@@ -25,7 +66,7 @@
 
             <ul class="texto">
                 
-                <li>Meu nome é Felipe Eduardo santana(tambem conhecido como crtl v)</li>
+                <li>Meu nome é Felipe Eduardo santana</li>
                 <li>Tenho 17 anos e estou no 3°ano do ensino medio</li>
                 <li>Gosto de ler, jogar, estudar e sou critico como hobby</li>
                 <li>Estudo programação no semear e no colégio
@@ -41,11 +82,25 @@
                 <li>Python</li>
                 <li>Programação em Arduino</li>
                 <li>Word,PowerPoint e Exel</li>
-                
+                <li>Esses são alguns projetos que eu criei:</li>
             </nav>
                 
             </ul>
         </div>
+        <!-- Definição do template -->
+    <template id="objetoTemplate">
+        <div class="objeto">
+            <img id="objetoImagem">
+            <h2 class="objetoNome"></h2>
+            <p class="objetoDesc"></p>
+            <a class="objetoLink" href=""></a>
+        </div>
+    </template>
+
+    <!-- Container para inserir os objetos -->
+    <div id="objetosContainer">
+
+    </div>
         
     </main>
     <footer>
